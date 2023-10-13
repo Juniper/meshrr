5232bbddf377153e5ecc2f56c299f1cfca0c362a
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2023-10-13 12:20:29 -0400

    Don't restart meshrr sidecars pending crpd

--- meshrr/connect_wait.py
+++ meshrr/connect_wait.py
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env python3
 
 # Copyright (c) Juniper Networks, Inc., 2023. All rights reserved.
-# 
+#
 # Notice and Disclaimer: This code is licensed to you under the MIT License (the
 # "License"). You may not use this code except in compliance with the License.
 # This code is not an official Juniper product.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,41 +29,21 @@
 # components is subject to the terms and conditions of the respective license as
 # noted in the Third-Party source code file.
 
-set -e
+from jnpr.junos import Device
+from jnpr.junos.exception import ConnectTimeoutError,ConnectRefusedError
+from time import sleep
 
-printenv > /etc/envvars
-
-if [ $# -eq 0 ]; then
-	echo "One of the following arguments required: init, sidecar"
-	exit 1
-elif [ $1 = 'init' ]; then
-	echo "Initializing pod"
-	# Overwrite with existing configuration if it exists.
-	if [ -f "/config/juniper.conf" ]; then
-		echo "Existing configuration detected; overwriting pod IP only."
-		sed "s/^\(.\+router-id\) [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/\1 {{ POD_IP }};/" /config/juniper.conf > juniper.conf.j2
-	else
-		echo "Initializing fresh configuration from template."
-	fi
-	# Generate a fresh SSH key and apply to configuration template.
-	ssh-keygen -q -t ed25519 -f /secret/ssh/id_ed25519 -P ""
-	PUBKEY=`cat \/secret\/ssh\/id_ed25519.pub | tr -d '\r\n'`
-	# If /opt/meshrr/overrides/juniper.conf.j2 exists, overwrite the default juniper.conf.j2 location.
-	if test -f overrides/juniper.conf.j2; then
-		cp overrides/juniper.conf.j2 juniper.conf.j2
-	fi
-	sed -i "/user meshrr/,/SECRET-DATA/ s~ssh-ed25519.*~ssh-ed25519 \"$PUBKEY\"; ## SECRET-DATA~" juniper.conf.j2
-	./render_config.py -i juniper.conf.j2 -o /config/juniper.conf
-elif [ $1 = 'sidecar' ]; then
-	# Wait for cRPD container to become available for netconf.
-	./connect_wait.py
-	echo "Initializing peer maintenance every ${UPDATE_SECONDS:=30} seconds."
-	while true; do
-		./update_peers.py
-		sleep ${UPDATE_SECONDS}
-	done
-else
-	echo "Invalid argument: $1"
-fi
-
-
+if __name__ == "__main__":
+    # Attempt to open a connection to the device
+    dev = Device(host="127.0.0.1",user="meshrr",ssh_private_key_file="/secret/ssh/id_ed25519")
+    while True:
+        try:
+            dev.open()
+            break
+        except ConnectTimeoutError:
+            print("connect_wait.py: Connection timed out; retrying.")
+        except ConnectRefusedError:
+            print("connect_wait.py: Connection refused; retrying.")
+            sleep(1)
+    # Create /tmp/connected-to-crpd to inform startup probes
+    open("/tmp/connected-to-crpd","x")
